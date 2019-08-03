dd65560723adae2b3d3006faf23ab6c02765bb0b
nguyenhachuy <gus.nguyen@mycase.com>
2019-08-03 15:34:46 -0700

    Player take_damage()

--- Player.gd
+++ Player.gd
@@ -76,8 +76,14 @@ func _on_Player_body_entered(body):
 	if 'mines' in body.get_groups():
 		body.detonate()
 
-	$HealthBar.update_current_health($HealthBar.current_health - 1)
-	emit_signal('hit')
+  take_damage()
 
 func _on_Player_area_entered(body):
 	pass
+
+func _on_Player_body_entered(body):
+	take_damage()
+
+func take_damage():
+	$HealthBar.update_current_health($HealthBar.current_health - 1)
+	emit_signal('hit')
