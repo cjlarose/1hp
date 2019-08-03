cec4cc5daa7b7dcb4524129957daf6ac23e8f770
Collin Styles <collin.styles@mycase.com>
2019-08-02 19:22:12 -0700

    Player takes damage when colliding with Enemy

--- Player.gd
+++ Player.gd
@@ -1,10 +1,15 @@
 extends Area2D
 
 export (int) var SPEED = 200
+export (int) var MAX_HEALTH = 100
+
+signal hit
+
+var health
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	health = MAX_HEALTH
 
 func _process(delta):
 	var direction = get_input_direction()
@@ -17,3 +22,7 @@ func get_input_direction():
 		int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")),
 		int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 	)
+
+func _on_Player_body_entered(body):
+	health -= 10
+	emit_signal('hit')
