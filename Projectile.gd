2ec9f704fd7f9a368d6ce93d8f7ff73f5d470182
Collin Styles <collin.styles@mycase.com>
2019-08-03 22:44:52 -0700

    Add pew pew sound effect

--- Projectile.gd
+++ Projectile.gd
@@ -1,12 +1,14 @@
 extends Area2D
 
 export (int) var SPEED = 20
+export (PackedScene) var PewPewSoundEffect
 
 var direction
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var sound_effect = PewPewSoundEffect.instance()
+	add_child(sound_effect)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
