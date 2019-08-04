b235e15abcd708119ec12d480f53dd5394b0ccec
Collin Styles <collin.styles@mycase.com>
2019-08-03 18:26:01 -0700

    Press Space to start the game

--- Title.gd
+++ Title.gd
@@ -9,9 +9,12 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+func _process(delta):
+	if Input.is_action_just_pressed('ui_accept'):
+		start_game()
 
 func _on_StartButton_pressed():
+	start_game()
+
+func start_game():
 	get_tree().change_scene('res://LevelMockup.tscn')
